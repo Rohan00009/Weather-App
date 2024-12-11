e02223337f1a721d8f790a0b3566513c757b7ac7
Rohan Kumar <104591914+Rohan00009@users.noreply.github.com>
2024-12-12 00:33:18 +0530

    Update API.js

--- src/helpers/API.js
+++ src/helpers/API.js
@@ -1,3 +1,3 @@
-export const weatherAppAPI = "f157f9cb1fd226939f8903319df356b6";
+export const weatherAppAPI = "3b1acba185012132db35c04ef7ce6b07";
 
 export const googleMapAPI = "AIzaSyAq15HbfCRMW7RqNb5LUNyOLyfzpYI0wl4";
